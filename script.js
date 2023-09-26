46e42728d2a6e0e56b626aa78f3842cc86b7bbf6
Ahmed Ramadan <ahmediramadan01@proton.me>
2023-09-26 17:18:27 +0300

    Store DOM elements in variables

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// DOM ELEMENTS //
+
+// Display Elements
+const displayPreviousElement = document.querySelector(".display__previous");
+const displayEqualElement = document.querySelector(".display__equal");
+const displayCurrentElement = document.querySelector(".display__current");
+const displayFirstElement = document.querySelector(".display__first");
+const displayOperatorElement = document.querySelector(".display__operator");
+const displaySecondElement = document.querySelector(".display__second");
+
+// ButtonsElements
+const clearEntryButtonElement = document.querySelector(".button--clear-entry");
+const clearButtonElement = document.querySelector(".button--clear");
+const numbersButtonsElements = document.querySelectorAll(".buttons--number");
+const operatorButtonElement = document.querySelectorAll(".button--operator");
+const decimalButtonElement = document.querySelector(".button--decimal");
+const equalButtonElement = document.querySelector(".button--equal");
